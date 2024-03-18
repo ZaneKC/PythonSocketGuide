1b6d697dc7a983fdb0740ef1f38c6e72c1ce2206
ZaneKC <88521824+ZaneKC@users.noreply.github.com>
2024-03-18 09:32:45 -0500

    Update serverheadless.py

--- Drafts/serverheadless.py
+++ Drafts/serverheadless.py
@@ -1,1 +1,28 @@
+import socket
+import datetime
+import time
+whatip = input("What ip would you like to host from?")
 
+def server_program():
+    host = whatip
+    port = 20007  # initiate port no above 1024
+    server_socket = socket.socket()  # get instance
+    server_socket.bind((host, port))  # bind host address and port together
+    server_socket.listen(5)
+    while True:
+        try:
+            conn, address = server_socket.accept()
+            while True:
+                try:
+                    data = conn.recv(1024).decode()
+                    print("from connected user: " + str(data))
+                    #Log Goes Here
+                    data = ("Message processed.")
+                    conn.send(data.encode())
+                except:
+                    print("Connection lost.")
+                    break
+        except:
+            print("No Conncetion.")
+
+server_program()
